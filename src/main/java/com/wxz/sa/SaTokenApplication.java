b07d2d2beb775df6e6a729bb8d2b0c1b8b01f24d
wxz <759126349@qq.com>
2024-06-20 10:21:08 +0800

    first commit

---
+++ src/main/java/com/wxz/sa/SaTokenApplication.java
@@ -0,0 +1,15 @@
+package com.wxz.sa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaTokenApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SaTokenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wxz/sa/SaTokenApplicationTests.java
@@ -0,0 +1,15 @@
+package com.wxz.sa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaTokenApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
